2ae9b51d20abc36f53899d69e9e5fbc5c9dd11ba
SuvanR <73233233+SuvanR@users.noreply.github.com>
2021-06-05 13:56:05 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,54 @@
+img = "";
+objects = [];
+status = "";
+
+function preload() {
+    img = loadImage('bedroom.jpeg');
+}
+
+function setup() {
+    canvas = createCanvas(500 , 370);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting object"; 
+}
+
+function modelLoaded() {
+    console.log("Model is loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    console.log(results);
+    if(error) {
+        console.error("Error");
+        window.alert("Error");
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+    document.getElementById("status").innerHTML = "Status : Status is detected";
+
+    image(img, 0, 0, 500, 400);
+
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Object is detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    } 
+}
+
+function back() {
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,23 @@
+function change_to_bedroom() {
+    window.location = "bedroom.html";
+}
+
+function change_to_ac() {
+    window.location = "ac.html";
+}
+
+function change_to_aquarium() {
+    window.location = "aquarium.html";
+}
+
+function change_to_calendar() {
+    window.location = "calendar.html";
+}
+
+function change_to_lamp() {
+    window.location = "lamp.html";
+}
+
+function back(){
+    window.location = "index.html";
+}
